44bba9d93645d17906daaba3549ff5d5f64764dc
Adarsh Sharma <adarsh0103sharma@gmail.com>
2025-09-24 18:55:59 +0530

    make some gui changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // your dev server port
-    proxy: {
-      // Proxy any request starting with /api to your backend
-      '/api': {
-        target: 'https://i-blog-nrg4.vercel.app',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api') // keep /api prefix
-      }
-    }
-  }
 })
